cade42dbfcff6479fe34947b75d2c72b457523da
MrNoOne007 <ramicmujo@hotmail.com>
2019-04-19 11:37:17 +0200

    Version 0.1

---
+++ get.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+	<title>Weather app <?php echo $_GET['Country'] ?></title>
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+</head>
+<Style>
+	html,body,h1,h2,h3,h4,h5,h6 {
+		font-family: "Arial", cursive, sans-serif;
+	}
+</Style>
+
+<body>
+	<?php
+	error_reporting(0);
+	$get = json_decode(file_get_contents('http://ip-api.com/json/'), true);
+
+	date_default_timezone_set($get['timezone']);
+	$city = $_GET['Country'];
+	$string = "http://api.openweathermap.org/data/2.5/weather?q=" . $city . "&units=metric&appid=420e9a5a8e9e1054b3fa434d69db2c75";
+	$data = json_decode(file_get_contents($string), true);
+	$temp = $data['main']['temp'];
+	$icon = $data['weather'][0]['icon'];
+
+	$Country = "<h1 class='w3-xxxlarge w3-animate-zoom'><b>" . $data['name'] . "," . $data['sys']['Country'] . "</h1></b>";
+
+	$logo = "<center><img src='http://openweathermap.org/img/w/" . $icon . ".png'></center>";
+	$desc = "<b><p>" . $data['weather'][0]['description'] . "</p></b>";
+
+	$temperature =  "<b>Temp:" . $temp . "°C</b><br>";
+	$clouds = "<b>Clouds:" . $data['clouds']['all'] . "%</b><br>";
+	$humidity = "<b>Humidity:" . $data['main']['humidity'] . "%</b><br>";
+	$pressure = "<b>Pressure:" . $data['main']['pressure'] . "hpa</b><br>";
+	$sunrise = "<b>Sunrise:" . date('h:i A', $data['sys']['sunrise']) . "</b><br>";
+	$sunset = "<b>Sunset:" . date('h:i A', $data['sys']['sunset']) . "</b>";
+	?>
+	<div class="w3-display-container w3-wide">
+		<img src="http://www.designbolts.com/wp-content/uploads/2014/03/Blue-Blur-Background1.jpg" style="width:100%;height:100%;" class="w3-image">
+		<div class="w3-display-topmiddle w3-margin-top">
+			<?php
+			echo $Country;
+			echo $logo;
+			echo "<center><h2>" . $desc . "</h1></center>";
+			?>
+		</div>
+		<div class="w3-display-middle w3-margin-top w3-padding-top">
+			<div class="w3-animate-left w3-margin-top"><br><br><br>
+				<h1 class="w3-xlarge">
+					<?php echo $temperature; ?>
+					<?php echo $clouds; ?>
+					<?php echo $humidity; ?>
+					<?php echo $pressure; ?>
+					<?php echo $sunrise; ?>
+					<?php echo $sunset; ?>
+					</h2>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<title>Weather App </title>
+</head>
+<body style="background-color: #9e9e9e3d; margin-top: 15%;"> 
+<center>
+<form method="GET" action="get.php">
+<h1>🅿🅻🅴🅰🆂🅴 🅴🅽🆃🅴🆁 🅳🅴🆂🅸🆁🅴🅳 🅲🅸🆃🆈 <h1>
+    <br>
+<p> 🅵🅾🆁 🅴🆇🅰🅼🅿🅻🅴 🆂🅰🆁🅰🅹🅴🆅🅾  </p>
+<br>
+<div style="display:inline-flex">
+<input type="text" name="Country" required style="margin-right:10px">
+<input class="btn btn-lg btn-primary" type="submit" value="Submit">
+</div>
+</form>
+</center>
+</body>
+</html>
